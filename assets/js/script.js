02b4ba91479a35c5a7d70fded8bbe06d14badfe1
Marina <marinakostova9222@gmail.com>
2024-03-06 18:13:06 +0000

    Add : added an if statement when the number of questions finish to go to the end page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionNumber = 0;
 let availableQuestions = [];
@@ -64,6 +64,9 @@ startGame = () => {
 };
 
 getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionNumber >= MAX_QUESTIONS) {
+    return window.location.assign("end.html"); // got to end page
+  }
   questionNumber++;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
@@ -72,6 +75,17 @@ getNewQuestion = () => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
 
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selecteAnswer = selectedChoice.dataset["number"];
+    getNewQuestion();
+  });
+});
 startGame();
