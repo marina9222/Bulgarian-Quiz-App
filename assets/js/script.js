ab8e543decd79187cbc6cfb7d79b79f9c6e275f8
Marina <marinakostova9222@gmail.com>
2024-03-09 15:01:02 +0000

    Fix : the correct and incorrect color was showing outside of the question container , bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,10 +82,21 @@ getNewQuestion = () => {
 choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
     if (!acceptingAnswers) return;
+
     acceptingAnswers = false;
     const selectedChoice = e.target;
-    const selecteAnswer = selectedChoice.dataset["number"];
-    getNewQuestion();
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
+
 startGame();
